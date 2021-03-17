211f77fec0d6b69d9b8784eaafa9e9e12fb6eec9
Nicole Goldschmidt <49738924+nicolegold@users.noreply.github.com>
2021-03-17 15:18:28 -0300

    Add files via upload

---
+++ CodigosAula/arquivo_wav/wav_plotar.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 12:07:03 2019
+
+@author: jeans
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import wave as fw
+
+arqff = 'sin_100Hz_1s.wav';
+#arqff = 'sin_1000Hz_1s.wav';
+#arqff = 'sin_10000Hz_1s.wav';
+arquivoWav = fw.open(arqff, 'r');
+
+print("Número canais: ", arquivoWav.getnchannels());
+print("Número bytes: ", arquivoWav.getsampwidth());
+print("Taxa de amostragem: ", arquivoWav.getframerate());
+print("Número de frames: ", arquivoWav.getnframes());
+print("Compactação: ", arquivoWav.getcompname());
+
+tipos = np.uint8;
+Damp = 256;
+if arquivoWav.getsampwidth()>1:
+    tipos = np.int16;
+    Damp = 32760;
+
+frames = arquivoWav.readframes(-1);
+Amplitude = np.fromstring(frames, tipos)/Damp;
+fim = np.size(Amplitude);
+fim2 = round(fim/2 - 1);
+
+dt = 1.0 / arquivoWav.getframerate();
+tempo = np.arange(0, arquivoWav.getnframes() * dt, dt);
+arquivoWav.close();
+
+plt.plot(tempo, Amplitude); plt.grid();
+plt.title(arqff);
+
+plt.show();
+
+
